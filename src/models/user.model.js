930243b92fb23f8600537181d770826d90bfcdc0
M Asad Munawar <masadmunawar915@gmail.com>
2023-09-08 11:38:24 +0500

    create the authentication APIs

---
+++ src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: String,
+    address: String,
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
